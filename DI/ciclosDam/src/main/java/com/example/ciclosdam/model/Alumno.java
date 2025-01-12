05cc0c7c9eb20dc9a273d4670fbddca592a0579f
xewi-11 <javi11coimbra@gmail.com>
2025-01-12 14:31:28 +0100

    Modelos de clases

---
+++ DI/ciclosDam/src/main/java/com/example/ciclosdam/model/Alumno.java
@@ -0,0 +1,17 @@
+package com.example.ciclosdam.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    // Constructor, getters y setters
+}
